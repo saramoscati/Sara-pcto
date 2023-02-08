dcff4795ce5df6e91fcc7719b8a9460e4cd8a1d0
saramoscati <124669286+saramoscati@users.noreply.github.com>
2023-02-08 09:50:02 +0100

    Add files via upload

---
+++ treno.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+    int anno;
+    printf("inserisci un numero\n");
+    scanf("%d", &anno);
+
+    if((anno % 4==0 && anno%100!=0) || anno%400==0)
+    {
+        printf("bisestile\n");
+    }
+
+    else 
+    {
+        printf("non bisestile\n");
+    }
+
+}
