5fad2495b429279e0412d167936ec0b16d214399
saramoscati <124669286+saramoscati@users.noreply.github.com>
2023-02-09 15:51:04 +0100

    Add files via upload

---
+++ laviniatvb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+int i = 0;
+int x=10;
+int array[11];
+
+while(i<=11)
+{
+array[i] = x;
+i = i+1;
+x= x+1;
+}
+
+i = 0;
+while (i<=11)
+{
+    printf("%d\n", array[i]);
+    i=i + 1;
+}
+
+
+
+
+
+}
