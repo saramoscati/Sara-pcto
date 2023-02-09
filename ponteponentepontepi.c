ac1af7221a0cafe23c8c4cce805f45f695892b9e
saramoscati <124669286+saramoscati@users.noreply.github.com>
2023-02-09 17:02:18 +0100

    Add files via upload

---
+++ ponteponentepontepi.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+
+int i = 0;
+int temp=9;
+int array[8];
+
+while(i<=8)
+{
+    scanf("%d", &array[i]);
+    i= i + 1;
+    
+}
+i = 0;
+while(i>=8)
+{
+temp= temp - 1;
+int temp 
+}
+i = 0;
+
+while(i<=8)
+{
+    printf("%d\n", array[i]);
+    i=i +1;
+
+}
+
+}
