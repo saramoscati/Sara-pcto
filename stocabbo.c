27a59c75d71fb6d8cd27ab6394f110d1bfcf87c3
saramoscati <124669286+saramoscati@users.noreply.github.com>
2023-02-09 10:09:24 +0100

    Add files via upload

---
+++ stocabbo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{ 
+int numero;
+int y=1;
+printf("inserisci un numero\n");
+scanf("%d", &numero);
+
+while(numero>0)
+{
+    y = numero * y;
+    numero = numero - 1;
+}  
+
+printf("il risultato e': %d\n", y);
+
+}
